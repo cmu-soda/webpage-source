fcf2a93b274f3a22135d1ec4d26630b5ffcf3c48
sychoo <cchu2@andrew.cmu.edu>
2021-10-22 14:45:05 -0400

    auto commit @ Fri Oct 22 14:45:05 EDT 2021

--- commit.sh
+++ commit.sh
@@ -1,17 +1,28 @@
 # Thu May 13 15:07:56 EDT 2021
+# Fri Oct 22 14:41:47 EDT 2021
 # automatically generate and commit the jekyll project
 
+# build and commit jekyll theme
 cd jekyll-theme
 jekyll build
+cd ..
 git add .
 git commit -m "auto commit @ $(date)"
 git push origin main
-cd ..
+echo "=================="
+echo "Jekyll Theme Built"
+echo "=================="
 
+# commit generated webpage files
 cd site
 git add .
 git commit -m "auto commit @ $(date)"
 git push origin main
+echo "======================="
+echo "Webpage Files Committed"
+echo "======================="
 
 echo
+echo "====="
 echo "Done!"
+echo "====="
